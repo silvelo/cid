827c157e84cc5c3d6ce1be6470f14b19d18403ea
Arturo Silvelo <arturo.silvelo@gmail.com>
2024-11-28 12:15:18 +0100

    fix: body rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 export default {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0] // Desactiva la regla de longitud de línea en el cuerpo
+  }
 };
